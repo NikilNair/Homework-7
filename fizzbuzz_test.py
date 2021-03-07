a653089d0695f29257109b110035c5acc40749f0
NikilNair <nairni@oregonstate.edu>
2021-03-07 13:50:38 -0800

    Wrote second test which checks if the function is appropriatley returning Fizz on multiples of three

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -8,5 +8,11 @@ class TestCase(unittest.TestCase):
         self.assertEqual(fizzbuzz.program(7), 7)
         self.assertEqual(fizzbuzz.program(8), 8)
 
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz.program(3), "Fizz")
+        self.assertEqual(fizzbuzz.program(6), "Fizz")
+        self.assertEqual(fizzbuzz.program(9), "Fizz")
+        self.assertEqual(fizzbuzz.program(12), "Fizz")
+
 if __name__ == '__main__':
     unittest.main()
