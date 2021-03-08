bfffdcc3f486bbaa875a74a93b9c2290867ca529
NikilNair <nairni@oregonstate.edu>
2021-03-07 19:44:52 -0800

    Wrote second test which checks if the function is appropriatley returning 'This is a leap year' on years evenly divisible by 4 but not evenly divisible by 100

--- leapyear_test.py
+++ leapyear_test.py
@@ -8,5 +8,11 @@ class TestCase(unittest.TestCase):
         self.assertEqual(leapyear.program(2009), "This is not a leap year")
         self.assertEqual(leapyear.program(2007), "This is not a leap year")
 
+    def test_div_by_4_and_no_100(self):
+        self.assertEqual(leapyear.program(2008), "This is a leap year")
+        self.assertEqual(leapyear.program(2012), "This is a leap year")
+        self.assertEqual(leapyear.program(2016), "This is a leap year")
+        self.assertEqual(leapyear.program(2020), "This is a leap year")        
+
 if __name__ == '__main__':
     unittest.main()
