d6a9396a9834a7f9f0facec130f704c32a1fe01b
NikilNair <nairni@oregonstate.edu>
2021-03-07 20:05:55 -0800

    Implemented appropriate functionality to return 'This is not a leap year' on years evenly divisible by 4 and 100 but not 400

--- leapyear.py
+++ leapyear.py
@@ -5,3 +5,5 @@ def program(val):
         return "This is a leap year"
     elif val % 4 == 0 and val % 100 == 0 and val % 400 == 0:
         return "This is a leap year"
+    elif val % 4 == 0 and val % 100 == 0 and val % 400 != 0:
+        return "This is not a leap year"    
